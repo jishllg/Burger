9373c1164033eabc64d83f789df40387e82064a2
Jishllg Laptop <jishllg@gmail.com>
2020-10-30 22:25:12 -0500

    Updated with nested callbacks.

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,30 @@
 let connection = require("../config/connection.js");
 
 let orm = {
-    selectAll: function() {
+    selectAll: function(sendUp) {
         connection.query("SELECT * FROM burgers;", function(err, result) {
             if (err) {
                 throw err;
             }
-            return result;
+            sendUp(result);
         });
     },
-    insertOne: function(name) {
+    insertOne: function(name, sendUp) {
         let query = "INSERT INTO burgers (name) VALUES (" + name + ");";
-        connection.query(query, function(err) {
+        connection.query(query, function(err, result) {
             if (err) {
                 throw err;
             }
+            sendUp(result);
         });
     },
-    updateOne: function(id) {
+    updateOne: function(id, sendUp) {
         let query = "UPDATE burgers SET devoured = true WHERE id = " + id + ";";
-        connection.query(query, function(err) {
+        connection.query(query, function(err, result) {
             if (err) {
                 throw err;
             }
+            sendUp(result);
         });
     }
 }
