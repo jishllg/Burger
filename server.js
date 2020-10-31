d19de94c83ba9777135487c4d62c34eed2941a34
Jishllg Laptop <jishllg@gmail.com>
2020-10-30 23:20:56 -0500

    Adjusted variable types.

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+let app = express();
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+const routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
 app.listen(PORT, function() {
